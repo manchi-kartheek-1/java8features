59675f4fceb361d88f5566c37d796856ab4cbfe9
Kartheek.Manchi <kartheek.manchi@tahrtech.com>
2026-06-23 10:20:49 +0530

    Updated StringPrograms

--- java8features/src/main/java/com/java8/features/easy/StringPrograms.java
+++ java8features/src/main/java/com/java8/features/easy/StringPrograms.java
@@ -2,6 +2,7 @@ package com.java8.features.easy;
 
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StringPrograms {
 
@@ -9,11 +10,21 @@ public class StringPrograms {
 
     static void main() {
         StringPrograms.countOccurrenceInString();
+        StringPrograms.convertingToUpperCase();
     }
 
     public static void countOccurrenceInString() {
-        Map<Character, Long> occurrences = testString.chars().mapToObj(c -> (char) c)
+        Map<Character, Long> occurrences = testString.chars()
+                .mapToObj(c -> (char) c)
                 .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
         System.out.println(occurrences);
+    }
+
+    public static void convertingToUpperCase() {
+        String string = testString.chars()
+                .mapToObj(c -> (char) Character.toUpperCase(c))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        System.out.println(string);
     }
 }
