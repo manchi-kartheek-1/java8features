15516ba7d99745f76688d9afcf8f96d6e7d9f9cc
Kartheek.Manchi <kartheek.manchi@tahrtech.com>
2026-06-25 18:01:29 +0530

    Updated ListPrograms

--- java8features/src/main/java/com/java8/features/easy/ListPrograms.java
+++ java8features/src/main/java/com/java8/features/easy/ListPrograms.java
@@ -3,18 +3,18 @@ package com.java8.features.easy;
 import java.util.Comparator;
 import java.util.List;
 
-public class FindHighestElement {
+public class ListPrograms {
 
     public static List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 22, 3, 44, 5, 666, 22);
 
     static void main() {
-        FindHighestElement.maxNumber();
-        FindHighestElement.minNumber();
+        ListPrograms.maxNumber();
+        ListPrograms.minNumber();
     }
 
     public static void maxNumber() {
 
-        Integer max = list.stream().distinct().min(Comparator.reverseOrder()).get();
+        Integer max = list.stream().distinct().max(Comparator.naturalOrder()).get();
         Integer secondHighest = list.stream().distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
         System.out.println(max);
         System.out.println(secondHighest);
@@ -22,7 +22,7 @@ public class FindHighestElement {
 
     public static void minNumber() {
 
-        Integer min = list.stream().distinct().max(Comparator.reverseOrder()).get();
+        Integer min = list.stream().distinct().min(Comparator.naturalOrder()).get();
         Integer secondMin = list.stream().distinct().sorted().skip(1).findFirst().get();
         System.out.println(min);
         System.out.println(secondMin);
