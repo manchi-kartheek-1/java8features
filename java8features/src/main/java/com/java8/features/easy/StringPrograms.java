73f67e441b8d0dd24831e37805a9a36357d4c097
Kartheek.Manchi <kartheek.manchi@tahrtech.com>
2026-06-25 17:29:41 +0530

    Updated StringPrograms

--- java8features/src/main/java/com/java8/features/easy/StringPrograms.java
+++ java8features/src/main/java/com/java8/features/easy/StringPrograms.java
@@ -2,13 +2,8 @@ package com.java8.features.easy;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Slf4j
 public class StringPrograms {
@@ -25,6 +20,11 @@ public class StringPrograms {
         StringPrograms.sortListFfStringsAlphabetically();
         StringPrograms.removeDuplicateStringsFromList();
         StringPrograms.FindFirstStringThatStartsWith();
+        StringPrograms.joinAllStringsIntoSingleCommaSeparatedString();
+        StringPrograms.findLongestStringInList();
+        StringPrograms.findShortestStringInList();
+        StringPrograms.filterStringsHavingLengthExactly();
+        StringPrograms.filterAllStringsAreNonEmpty();
     }
 
     public static void countOccurrenceInString() {
@@ -92,8 +92,43 @@ public class StringPrograms {
 
     public static void FindFirstStringThatStartsWith() {
         String string = "principle + charges = installment amount or else report and don't add customer installments";
-        String s= Arrays.stream(string.split(" ")).filter(word -> word.startsWith("i")).findFirst().get();
+        String s = Arrays.stream(string.split(" ")).filter(word -> word.startsWith("i")).findFirst().get();
         System.out.println(s);
+    }
+
+    public static void joinAllStringsIntoSingleCommaSeparatedString() {
+        String string = "principle + charges = installment amount or else report and don't add customer installments";
+        String collect = Arrays.stream(string.split(" ")).map(String::valueOf).collect(Collectors.joining(","));
+        System.out.println(collect);
+    }
+
+    public static void findLongestStringInList() {
+        String string = "principle + charges = installment amount or else report and don't add customer installmentsss";
+        Map<String, Integer> stringIntegerMap = Arrays.stream(string.split(" ")).max(Comparator.comparingInt(String::length))
+                .map(word -> Collections.singletonMap(word, word.length()))
+                .orElse(Collections.emptyMap());
+        System.out.println(stringIntegerMap);
+    }
+
+    public static void findShortestStringInList() {
+        String string = "principle charges installment amount or else report and don't add customer installmentsss";
+        Map<String, Integer> stringIntegerMap = Arrays.stream(string.split(" "))
+                .min(Comparator.comparingInt(String::length))
+                .map(word -> Collections.singletonMap(word, word.length()))
+                .orElse(Collections.emptyMap());
+        System.out.println(stringIntegerMap);
+    }
+
+    public static void filterStringsHavingLengthExactly() {
+        String string = "principle charges installment amount or else report and don't add customer installments";
+        List<String> list = Arrays.stream(string.split(" ")).filter(word -> word.length() == 5).toList();
+        System.out.println(list);
+    }
+
+    public static void filterAllStringsAreNonEmpty() {
+        List<String> list = List.of("principle", "charges", "installment", "", " ", "else", "report", "and", "don't", "add", "customer");
+        List<String> collect = list.stream().filter(word -> word.isEmpty() && word.isBlank()).collect(Collectors.toList());
+        System.out.println(collect);
     }
 
 }
