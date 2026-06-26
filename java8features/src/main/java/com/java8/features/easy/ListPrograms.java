d4e1babc0d77b25f9aad5dcdc72f6f58c64a23e6
kartheek-manchi-sde <kartheekmanchise@gmail.com>
2026-06-26 08:39:30 +0530

    Updated ListPrograms

--- java8features/src/main/java/com/java8/features/easy/ListPrograms.java
+++ java8features/src/main/java/com/java8/features/easy/ListPrograms.java
@@ -10,6 +10,21 @@ public class ListPrograms {
     static void main() {
         ListPrograms.maxNumber();
         ListPrograms.minNumber();
+        ListPrograms.findAllEvenNum();
+        ListPrograms.findAllEvenOdd();
+        ListPrograms.squareEachElementInList();
+        ListPrograms.sumEachElementInList();
+        ListPrograms.findMaxInList();
+        ListPrograms.findMinInList();
+        ListPrograms.countTotElements();
+        ListPrograms.removeDuplicatesInList();
+        ListPrograms.ascList();
+        ListPrograms.dscList();
+        ListPrograms.findNumGGivenNum();
+        ListPrograms.findNumLGivenNumber();
+        ListPrograms.doubleNumInList();
+        ListPrograms.findAnyNumIsNeg();
+        ListPrograms.findAllNumIsPov();
     }
 
     public static void maxNumber() {
@@ -26,6 +41,89 @@ public class ListPrograms {
         Integer secondMin = list.stream().distinct().sorted().skip(1).findFirst().get();
         System.out.println(min);
         System.out.println(secondMin);
+    }
+
+    public static void findAllEvenNum(){
+        List<Integer> list1 = list.stream().filter(i -> i % 2 == 0).toList();
+        System.out.println(list1);
+    }
+
+    public static void findAllEvenOdd(){
+        List<Integer> list1 = list.stream().filter(i -> i % 2 == 1).toList();
+        System.out.println(list1);
+    }
+
+    public static void squareEachElementInList() {
+        List<Integer> list1 = list.stream().map(i -> i * i).toList();
+        System.out.println(list1);
+    }
+
+    public static void sumEachElementInList() {
+        int sum = list.stream().mapToInt(Integer::valueOf).sum();
+        Integer reduce = list.stream().reduce(0, Integer::sum);
+        System.out.println(reduce);
+        System.out.println(sum);
+    }
+
+    public static void findMaxInList(){
+        Integer i = list.stream().sorted(Comparator.reverseOrder()).findFirst().get();
+        Integer i1 = list.stream().max(Comparator.naturalOrder()).get();
+        System.out.println(i);
+        System.out.println(i1);
+    }
+
+    public static void findMinInList(){
+        Integer i = list.stream().sorted().findFirst().get();
+        Integer i1 = list.stream().min(Comparator.naturalOrder()).get();
+        System.out.println(i);
+        System.out.println(i1);
+    }
+
+    public static void countTotElements(){
+        long count = list.stream().count();
+        int size = list.size();
+        System.out.println(count);
+        System.out.println(size);
+    }
+
+    public static void removeDuplicatesInList(){
+        List<Integer> list1 = list.stream().distinct().toList();
+        System.out.println(list1);
+    }
+
+    public static void ascList(){
+        List<Integer> list1 = list.stream().distinct().sorted().toList();
+        System.out.println(list1);
+    }
+
+    public static void dscList(){
+        List<Integer> list1 = list.stream().distinct().sorted(Comparator.reverseOrder()).toList();
+        System.out.println(list1);
+    }
+
+    public static void findNumGGivenNum(){
+        List<Integer> list1 = list.stream().filter(i -> i > 50).toList();
+        System.out.println(list1);
+    }
+
+    public static void findNumLGivenNumber(){
+        List<Integer> list1 = list.stream().filter(i -> i < 50).toList();
+        System.out.println(list1);
+    }
+
+    public static void doubleNumInList(){
+        List<Integer> list1 = list.stream().map(i -> i + 1).toList();
+        System.out.println(list1);
+    }
+
+    public static void findAnyNumIsNeg(){
+        boolean b = list.stream().anyMatch(i -> i < 0);
+        System.out.println(b);
+    }
+
+    public static void findAllNumIsPov(){
+        boolean b = list.stream().allMatch(i -> i > 0);
+        System.out.println(b);
     }
 
 
