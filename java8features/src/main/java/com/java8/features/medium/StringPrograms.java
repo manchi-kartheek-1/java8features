bf5c402ea350d251389870777f9f55cf1b634129
kartheek-manchi-sde <kartheekmanchise@gmail.com>
2026-06-29 09:55:26 +0530

    Updated String programs

--- java8features/src/main/java/com/java8/features/medium/StringPrograms.java
+++ java8features/src/main/java/com/java8/features/medium/StringPrograms.java
@@ -5,12 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class StringPrograms {
 
     public static String string = "ea principle + charges = installment amount or else report and don't add customer installment";
-
+    public static String string1 = "Java Programming";
 
     static void main() {
         StringPrograms.findAllWordsGNNdSort(6);
@@ -22,6 +23,12 @@ public class StringPrograms {
         StringPrograms.sortStringsByLength();
         StringPrograms.findMostOccurringString();
         StringPrograms.convertListOfStringsToMapOfStringsAndLen();
+        StringPrograms.findDuplicateCharsInString();
+        StringPrograms.firstNonRepeatingCharacterInString();
+        StringPrograms.firstRepeatedCharInString();
+        StringPrograms.reverseString();
+        StringPrograms.findFrequencyOfEachChar();
+        StringPrograms.findLongestString();
     }
 
     public static void findAllWordsGNNdSort(int i) {
@@ -89,5 +96,56 @@ public class StringPrograms {
         System.out.println(collect);
     }
 
+    public static void findDuplicateCharsInString() {
+        Set<Character> characters = new HashSet<>();
+        List<Character> list = string1.chars().mapToObj(c -> (char) c)
+                .filter(c -> !characters.add(c))
+                .toList();
+        System.out.println(list);
+    }
+
+    public static void firstNonRepeatingCharacterInString() {
+        Character c1 = string1.chars().mapToObj(c -> (char) c)
+                .distinct()
+                .findFirst()
+                .get();
+        System.out.println(c1);
+    }
+
+
+    public static void firstRepeatedCharInString() {
+        Set<Character> characters = new HashSet<>();
+        Character list = string1.chars().mapToObj(c -> (char) c)
+                .filter(c -> !characters.add(c))
+                .findFirst().get();
+        System.out.println(list);
+    }
+
+    public static void reverseString() {
+        String collect = IntStream.range(0, string1.length())
+                .mapToObj(i -> string1.charAt(string1.length() - 1 - i))
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        System.out.println(collect);
+    }
+
+    public static void findFrequencyOfEachChar() {
+
+        Map<Character, Long> collect = string1.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(collect);
+    }
+
+    public static void findLongestString() {
+//        Integer i =
+        Map.Entry<Integer, List<String>> i = Arrays.stream(string.split(" "))
+                .collect(Collectors.groupingBy(String::length))
+                .entrySet()
+                .stream().max(Map.Entry.comparingByKey())
+//                .map(Map.Entry::getKey)
+                .get();
+        System.out.println(i);
+    }
 
 }
